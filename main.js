cebe0bfae0b4085177c4e7abe2ee7673a953988b
Mostafa S. El-bastawisy <mostafa.sayed112@gmail.com>
2021-07-29 14:35:18 +0200

    Add JavaScript Bootcamp from 023 to 026 Soul

---
+++ main.js
@@ -0,0 +1,19 @@
+let a = "Elzero Web School";
+
+console.log(); //Zero
+
+// 8 H
+console.log(); //HHHHHHHHHH
+
+// Return Arry 
+
+console.log(); // [Elzero]
+
+// Use Only "Subtr" Method + Template Literals in Your Solution
+
+console.log(); //Elezero School
+
+// Solution Must Be Dynamic And String May Change 
+
+console.log(); // eLZERO WEB SCHOOL
+
